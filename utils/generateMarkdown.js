1b5f95bef6e449247358a94a7aca89807d50d097
Lisa Jean <MsJean@Lisas-Air.fios-router.home>
2022-02-20 15:46:59 -0500

    added license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,18 +10,24 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return ` # ${license}
-  `; //put link in []
-  // http://www.apache.org/licenses/
-  // https://creativecommons.org/licenses/
-  // https://www.isc.org/licenses/
+  if (license !== "None") {
+    return ` ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0);
+           ![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC);
+           [![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)
+`;
+  }
+  return "";
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return ` #${license}
+  if (license !== "None") {
+    return ` #${license}
+
 `;
+  }
+  return "";
 }
 
 // TODO: Create a function to generate markdown for README
